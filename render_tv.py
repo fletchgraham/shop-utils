d55208fbf634f8889cc500b0bf32306620546030
fletchgraham <sfletchgraham@gmail.com>
2023-08-24 14:13:43 -0600

    save jpegs for tv

--- render_tv.py
+++ render_tv.py
@@ -12,9 +12,10 @@ scene = bpy.context.scene
 scene.render.resolution_x = 3840
 scene.render.resolution_y = 2160
 
-bpy.context.scene.render.image_settings.file_format = 'PNG'
+bpy.context.scene.render.image_settings.file_format = 'JPEG'
+bpy.context.scene.render.image_settings.quality = 100
 
-img_name = f"{blend_name}_src"
+img_name = blend_name
 scene.render.filepath = (blend_path.parent / blend_name / img_name).as_posix()
 
 bpy.ops.render.render(write_still=True)
